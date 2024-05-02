b5b310d969e6d86caa45db6d33f343aaf5d83434
Phantomlancer123 <fronzen615heart@gmail.com>
2024-05-02 15:26:48 +0200

    feat: complete html parsing function

--- index.js
+++ index.js
@@ -23,5 +23,45 @@ if (!url) {
 })();
 
 function parseHtmlToJson($) {
-    return $;
+  const json = {
+    title: $('div.title h1').text(),
+    subtitles: $('div.subtitle').map((index, element) => {
+      const $element = $(element);
+      return {
+        id: $element.attr('id'),
+        title: $element.find('h2').text(),
+        parts: $element.find('div.part').map((index, partElement) => {
+          const $partElement = $(partElement);
+          return {
+            id: $partElement.attr('id'),
+            title: $partElement.find('h1 a').text(),
+            subparts: $partElement.find('div.subpart').map((index, subpartElement) => {
+              const $subpartElement = $(subpartElement);
+              return {
+                id: $subpartElement.attr('id'),
+                title: $subpartElement.find('h2 a').text(),
+                sections: $subpartElement.find('div.section').map((index, sectionElement) => {
+                  const $sectionElement = $(sectionElement);
+                  return {
+                    id: $sectionElement.attr('id'),
+                    title: $sectionElement.find('h4 a').text(),
+                    paragraphs: $sectionElement.find('p').map((index, paragraphElement) => {
+                      const $paragraphElement = $(paragraphElement);
+                      return {
+                        text: $paragraphElement.text(),
+                        indentLevel: ['indent-1', 'indent-2', 'indent-3', 'indent-4', 'indent-5', 'indent-6']
+                          .findIndex(className => $paragraphElement.hasClass(className)) + 1
+                      };
+                    }).get()
+                  };
+                }).get()
+              };
+            }).get()
+          };
+        }).get()
+      };
+    }).get()
+  };
+
+  return json;
 }
